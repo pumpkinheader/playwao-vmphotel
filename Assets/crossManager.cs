0ffcedfd61c1850eab1d716c5acbc9756f053613
pumpkinheader <pumpkinde@gmail.com>
2014-12-27 02:21:53 +0900

    change cross image

--- Assets/crossManager.cs
+++ Assets/crossManager.cs
@@ -3,23 +3,18 @@ using System.Collections;
 
 public class crossManager : MonoBehaviour {
 
-		GameObject cross_r = new GameObject();
-		GameObject cross_c = new GameObject();
-		SpriteRenderer sr_r= new SpriteRenderer();
-		SpriteRenderer sr_c = new SpriteRenderer();
+		GameObject cross_s = new GameObject();
+		SpriteRenderer sr_s= new SpriteRenderer();
+
 
 
 	// Use this for initialization
 	void Start () {
 				GameSceneManager.gmscript.cross = this.gameObject;
-				cross_r = GameObject.Find ("cross_r");
-				cross_c = GameObject.Find ("cross_c");
-				sr_r = cross_r.GetComponent<SpriteRenderer> ();
-				sr_c = cross_c.GetComponent<SpriteRenderer> ();
-				sr_c.enabled = false;
-				sr_r.enabled = false;
-				iTween.ColorTo (cross_r,iTween.Hash("a",0f,"time",0.01f));
-				iTween.ColorTo (cross_c,iTween.Hash("a",0f,"time",0.01f));
+				cross_s = GameObject.Find ("cross_r");
+				sr_s = cross_s.GetComponent<SpriteRenderer> ();
+				sr_s.enabled = false;
+				iTween.ColorTo (cross_s,iTween.Hash("a",0f,"time",0.01f));
 	}
 	
 	// Update is called once per frame
@@ -28,21 +23,17 @@ public class crossManager : MonoBehaviour {
 	}
 
 		void visible(int type){
-				sr_c.enabled = true;
-				sr_r.enabled = true;
+				sr_s.enabled = true;
 				//iTween.ColorTo (cross_r,iTween.Hash("a",0.9f,"time",0.6f,"easetype",iTween.EaseType.easeOutExpo));
 				//iTween.ColorTo (cross_c,iTween.Hash("a",0.9f,"time",0.6f,"easetype",iTween.EaseType.easeOutExpo));
 				//iTween.ColorTo(cross_c,iTween.Hash("a",1f,"delay",0.4f,"looptype","pingpong","easetype","easeincirc","time",0.8f));
 				//iTween.ColorTo(cross_r,iTween.Hash("a",1f,"delay",0.4f,"looptype","pingpong","easetype","easeincirc","time",0.8f));
-				iTween.ColorTo(cross_c,iTween.Hash("a",1f,"delay",0.4f,"easetype","easeincirc","time",2.6f,"oncompletetarget",gameObject,"oncomplete","hide"));
-				iTween.ColorTo(cross_r,iTween.Hash("a",1f,"delay",0.4f,"easetype","easeincirc","time",2.6f,"oncompletetarget",gameObject,"oncomplete","hide"));
+				iTween.ColorTo(cross_s,iTween.Hash("a",1f,"delay",0.4f,"easetype","easeincirc","time",2.6f,"oncompletetarget",gameObject,"oncomplete","hide"));
 				deq ();
 		}
 		void hide(){
-				sr_c.enabled = false;
-				sr_r.enabled = false;
-				iTween.ColorTo (cross_r,iTween.Hash("a",0f,"time",0.01f));
-				iTween.ColorTo (cross_c,iTween.Hash("a",0f,"time",0.01f));
+				sr_s.enabled = false;
+				iTween.ColorTo (cross_s,iTween.Hash("a",0f,"time",0.01f));
 				//deq ();
 		}
 
